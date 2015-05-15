fd6da6eed717c8167cd516c3a96a063d675db19d
Sarath Babu <sarathbabu@agileblaze.email>
2015-05-15 20:32:59 +0530

    otp

---
+++ otp/password.py
@@ -0,0 +1,97 @@
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import calendar
+import time
+
+from keystone.auth.plugins.password import UserAuthInfo
+from keystone.common import dependency
+from keystone import auth
+from keystone import exception
+from keystone.openstack.common.gettextutils import _
+from keystone.openstack.common import log
+
+import exceptions
+import sql
+import twilio_api
+
+
+METHOD_NAME = 'password_otp'
+
+LOG = log.getLogger(__name__)
+
+class UserOtpAuthInfo(object):
+    @staticmethod
+    def create(user_id):
+        otp_auth_info = UserOtpAuthInfo(user_id)
+        return otp_auth_info
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        
+    def validate(self):
+        otp_info  = sql.get_otp_auth_status(self.user_id)
+        if otp_info:
+            if otp_info.successive_failures > 2:
+                time_blocked_from = otp_info.last_failure_timestamp
+                blocked_from_epoch = calendar.timegm(time.strptime(str(time_blocked_from), '%Y-%m-%d %H:%M:%S'))
+                blocked_period_seonds = self.epoch_time_difference(time.time(), blocked_from_epoch)
+                if self.is_account_blocked(blocked_period_seonds):
+                    blocked_period = self.seconds_to_hrs_mins_secs(86400 - blocked_period_seonds)
+                    error_message = "Your accound is blocked for 24 hours, wait %d hours %d minutes %d seconds to get the account reactivated" % (blocked_period[0], blocked_period[1], blocked_period[2])
+                    raise exceptions.Blocked(error_message)
+            
+    def epoch_time_difference(self, later_time, former_time):
+        return later_time - former_time
+    
+    def seconds_to_hrs_mins_secs(self, seconds):
+        minutes , seconds  = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return hours, minutes, seconds
+    
+    def is_account_blocked(self,blocked_period_seconds):
+        if blocked_period_seconds >= 86400 :
+            return False
+        else:
+            return True
+        
+
+@dependency.requires('identity_api')
+class Password(auth.AuthMethodHandler):
+
+    method = METHOD_NAME
+
+    def authenticate(self, context, auth_payload, user_context):
+        """Try to authenticate against the identity backend."""
+        user_info = UserAuthInfo.create(auth_payload)
+        otp_auth_info = UserOtpAuthInfo(user_info.user_id)
+
+        try:
+            self.identity_api.authenticate(
+                context,
+                user_id=user_info.user_id,
+                password=user_info.password,
+                domain_scope=user_info.domain_id)
+            
+            otp_auth_info.validate()
+            twilio_api.send_otp(user_info.user_id)
+            
+        except AssertionError:
+            msg = _('Invalid username or password')
+            raise exception.Unauthorized(msg)
+        except exceptions.Blocked as user_blocked_exception:
+            raise exception.Unauthorized(user_blocked_exception.message)
+
+        if 'user_id' not in user_context:
+            user_context['user_id'] = user_info.user_id
---
+++ otp/utils.py
@@ -0,0 +1,133 @@
+import config
+import hashlib
+import hmac
+import os
+import struct
+import time
+import uuid
+
+"""Functions for generating and validaing OTP
+
+The method used for one-time password generation is RFC6238-TOTP.
+For more information on RFC6238 please visit
+https://tools.ietf.org/html/rfc6238
+"""
+
+CONF = config.CONF
+
+def _get_time_step():
+    """Gets the time-step for the TOTP
+    """
+    
+    return CONF.otp_options.time_step
+    
+def _get_max_time_step_window():
+    """Number of time-step windows considered backwards for OTP validation
+    
+    A one-time password could be received on the next time-step window
+    rather the same time-step window in which it generated. So at the time
+    of validation we could check the validity of the token by considering
+    time-step windows which preceeds the current one
+    
+    If the OTP is valid for any of the considered time-step window we take
+    the OTP as a valid one.
+    
+    The number of time-step windows need to consider can be set in the
+    configuration file.
+    """
+    
+    return CONF.otp_options.time_step_window_back
+
+    
+def generate_totp(secret):
+    """Generate the TOTP using secret
+    
+    :param secret: the secret of the user
+    
+    :returns totp: the one-time password
+    """
+    
+    #code to generate otp based on unix time
+    unix_time_synced = get_time_step_window(0)
+    hmac_digest = hmac.new(bytes([unix_time_synced]), secret, digestmod = hashlib.sha256)
+    
+    totp = (struct.unpack("16i",bytearray(hmac_digest.hexdigest()))[15]+1000000) % 1000000
+    
+    #return otp
+    return  totp
+    
+    
+def get_time_step_window(position = 0):
+    """Returns the starting unix-time of the step-time window
+    
+    This function can be used to get the begining unix-time for the
+    time-step window which comes before the current time-step window
+    
+    Position 0 means the current time-step window
+    
+    :param position: position of the time-step window before the
+                     current time-step window
+    """
+
+    sync_time = get_current_unix_time() - _get_time_step() * position
+    return  sync_time - (sync_time % _get_time_step())
+    
+
+def get_current_unix_time():
+    """Returns the current unix time
+    """
+    
+    return int(time.time())
+
+def validate_totp(secret, totp):
+    """Validates the one-time password using the secret
+    
+    For each time-step window specified for validation this function
+    will generate an TOTP and will compare with the received TOTP
+    if a match found for any of the generated TOTP the function
+    treat that OTP as a valid one
+    
+    If the validation fails for all the generated TOTPs the function
+    generates Exception
+    
+    :param secret: the secret of the user from whom the OTP is received
+    :param otp: one-time password received from the user
+    
+    :returns None: if the validation is okay
+    
+    :raises AssertionError: if the validation fails
+    """
+
+    for position in range(0, _get_max_time_step_window()):
+        otp_for_validation = generate_totp(secret)
+        if otp_for_validation == int(totp) :
+            return None
+    raise AssertionError  
+
+def generate_secret():
+    
+    return os.urandom(24)
+
+def generate_uuid4_string():
+    """Generate a UUID string
+    """
+    
+    return uuid.uuid4().get_hex()
+
+def get_current_mysql_gmtime():
+    """Generate the current time in MYSQL time format
+    """
+    
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(get_current_unix_time()))
+
+
+
+    
+    
+    
+    
+        
+        
+     
+        
+    
