d40067ae0d49846ebf8186da59d99977723d48c7
Sarath Babu <sarathbabu@agileblaze.email>
2015-05-20 21:12:07 +0530

    test modified

--- otp/test_v3_otp_auth.py
+++ otp/test_v3_otp_auth.py
@@ -23,8 +23,18 @@ import mock
 from oslo_utils import timeutils
 import six
 from testtools import testcase
+from oslo_config import cfg
 
+from sqlalchemy.orm import sessionmaker
+
+
+from keystone.auth import controllers as auth_controllers
+from keystone.common import dependency
 from keystone import auth
+from keystone import resource
+
+from keystone.tests import unit as tests
+from keystone.tests.unit.ksfixtures import database
 from keystone.tests.unit import test_v3
 from keystone.tests.unit import test_v3_auth
 from keystone.tests.unit import default_fixtures
@@ -34,23 +44,31 @@ from keystone.auth.plugins.otp import config
 from keystone.auth.plugins.otp import sql
 
 
-from  oslo_config import cfg
+from testtools import TestCase
 
-CONF = cfg.CONF
+from webtest import TestApp
+from webtest import AppError
 
 
-class TestOtpAuthInfo(test_v3.RestfulTestCase):
+app = TestApp('http://*:35357')
+test_user_id = 'd5072ef18e844852810150a217a3d496'
+
+class TestOtpAuthInfo(TestCase):
+    """ This test class uses the ruuning keystone WSGI application in the local system
+    
+    It uses live running system, not using any virtual environments
+   
+    Before running this tests you have to do the following
+   
+    1. Create a user "admin" with domain "default"
+    
+    In this test file change the attribute  "test_user_id" to the id of the user
+    
+    
+    """
     
     def setUp(self):
-    
-        #super(TestOtpAuthInfo, self).setUp()
-    #    auth.controllers.load_auth_methods()
-    #    #self.load_fixtures(default_fixtures)
-    #    sql.add_phone_number(self.user_id, CONF.otp_options.twilio_from_phone)
-        
-
-      
-       
+        super(TestOtpAuthInfo, self).setUp()
 
     def build_authentication_request(self, user_id=None,
                                      username=None, user_domain_id=None,
@@ -73,12 +91,12 @@ class TestOtpAuthInfo(test_v3.RestfulTestCase):
         if password_otp:
             auth_data['identity']['methods'].append('password_otp')
             auth_data['identity']['password_otp'] = self.build_password_auth(
-                user_id, username, user_domain_id, user_domain_name, password)
+                user_id, username, user_domain_id, user_domain_name, password_otp)
             
         if one_time_password:
             auth_data['identity']['methods'].append('otp')
             auth_data['identity']['otp'] = self.build_password_auth(
-                user_id, username, user_domain_id, user_domain_name, password)
+                user_id, username, user_domain_id, user_domain_name, one_time_password)
 
         if kwargs:
             auth_data['scope'] = self.build_auth_scope(**kwargs)
@@ -86,62 +104,102 @@ class TestOtpAuthInfo(test_v3.RestfulTestCase):
     
     
     def test_valid_otp(self):
-        otp = utils.generate_totp(sql.get_secret(self.user['id']))
-        
+      
+        print "Running correct OTP Test \n"
+
+        otp = utils.generate_totp(sql.get_secret(test_user_id))
         auth_data = self.build_authentication_request(
-            username=self.user['name'],
-            user_domain_id=self.domain_id,
-            password=self.user['password'],
-            one_time_password=otp)
+            username='admin',
+            user_domain_id='default',
+            password='password',
+            one_time_password=str(otp))
         
-        return self.post('/auth/tokens', body=auth_data, expected_status=201)
+        auth_data = json.dumps(auth_data)
+        return self.new_post(url = '/v3/auth/tokens', body=auth_data, expected_status=201)
     
     def test_invalid_otp(self):
-        otp = utils.generate_totp(sql.get_secret(self.user_id))
+      
+        print "Running invalid OTP Test \n"
+        
+        otp = utils.generate_totp(sql.get_secret(test_user_id))
         otp = otp - 1
         
         auth_data = self.build_authentication_request(
-            username=self.user['name'],
-            user_domain_id=self.domain_id,
-            password=self.user['password'],
-            one_time_password=otp)
+            username='admin',
+            user_domain_id='default',
+            password='password',
+            one_time_password=str(otp))
         
-        return self.post('/auth/tokens', body=auth_data, expected_status=401)
+        auth_data = json.dumps(auth_data)
+        return self.new_post(url = '/v3/auth/tokens', body=auth_data, expected_status=201)
     
     def test_expired_otp(self):
-        otp = utils.generate_totp(sql.get_secret(self.user_id), positions_behind = 3)
+        
+        print "Running expired OTP Test \n"
+      
+        otp = utils.generate_totp(sql.get_secret(test_user_id), position = 3)
         
         auth_data = self.build_authentication_request(
-            username=self.user['name'],
-            user_domain_id=self.domain_id,
-            password=self.user['password'],
-            one_time_password=otp)
+            username='admin',
+            user_domain_id='default',
+            password='password',
+            one_time_password=str(otp))
         
-        return self.post('/auth/tokens', body=auth_data, expected_status=401)
-    
+        auth_data = json.dumps(auth_data)
+        
+        return self.new_post('/v3/auth/tokens', body=auth_data, expected_status=401)
+     
     def test_blocked_user(self):
-        otp = utils.generate_totp(sql.get_secret(self.user_id))
-        
+      
+        print "Running blocked user Test \n"
+      
         auth_data = self.build_authentication_request(
-            username=self.user['name'],
-            user_domain_id=self.domain_id,
-            password_otp=self.user['password'])
+            username='admin',
+            user_domain_id='default',
+            password_otp='password')
         
-        return self.post('/auth/tokens', body=auth_data, expected_status=401)
+        auth_data = json.dumps(auth_data)
         
-        
+        return self.new_post('/v3/auth/tokens', body=auth_data, expected_status=401)
+         
+         
     def test_send_sms(self):
-        otp = utils.generate_totp(sql.get_secret(self.user_id))
-        
+      
+        print "Running send SMS Test \n"
+      
         auth_data = self.build_authentication_request(
-            username=self.user['name'],
-            user_domain_id=self.domain_id,
-            password_otp=self.user['password'])
+            username='admin',
+            user_domain_id='default',
+            password_otp='password')
         
-        return self.post('/auth/tokens', body=auth_data, expected_status=201)
-        
-        
-        
+        auth_data = json.dumps(auth_data)
+        return self.new_post('/v3/auth/tokens', body=auth_data, expected_status=201)
+
+    
+    def new_post(self, url=None, body=None, expected_status=None):
+      try:
+         response = app.post(url, body, content_type='application/json')
+         print response
+      except AppError as e:
+         print e.message
+      
+      
+    def build_password_auth(self, user_id=None, username=None,
+                            user_domain_id=None, user_domain_name=None,
+                            password=None):
+        password_data = {'user': {}}
+        if user_id:
+            password_data['user']['id'] = user_id
+        else:
+            password_data['user']['name'] = username
+            if user_domain_id or user_domain_name:
+                password_data['user']['domain'] = {}
+                if user_domain_id:
+                    password_data['user']['domain']['id'] = user_domain_id
+                else:
+                    password_data['user']['domain']['name'] = user_domain_name
+        password_data['user']['password'] = password
+        return password_data
     
         
         
